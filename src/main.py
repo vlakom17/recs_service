4e267d09d513a498383b3a22ad4919e915460135
Vladislav Kombarov <133577853+vlakom17@users.noreply.github.com>
2024-12-27 16:32:01 +0300

    улучшен сервис рекомендаций

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ from src.routes import recommend
 from src.core.dependencies import setup_dependencies
 from src.core.config import cfg
 from src.core.logging import LOGGING
-from rabbitmq_consumer import consume_rabbitmq
 from consumer import consume
 import asyncio
 import logging
